5b408c25c79f00bdf33a8ed6b7ae96fa859d8b2a
longqt321 <longqt321@gmail.com>
2024-02-20 23:43:31 +0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".navbar_links");
+const menuButton = document.querySelector(".navbar_icons");
+
+menuButton.addEventListener("click", ()=>{
+    menu.classList.toggle("navbar_open");
+});
